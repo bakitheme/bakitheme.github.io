94045143b513c0e33097d2b37a99fbec853130c8
Denis Vakar <gersi00057@gmail.com>
2020-05-19 17:54:24 +0300

    first

---
+++ js/common.js
@@ -0,0 +1,13 @@
+$(function() {
+	var containerEl = document.querySelector('.container');
+
+    var mixer = mixitup(containerEl);
+
+    $('.owl-carousel').owlCarousel({
+        loop:true,
+        margin:10,
+        nav:true,
+        navText:['Previous','Next'],
+        items: 1,
+    })
+});
